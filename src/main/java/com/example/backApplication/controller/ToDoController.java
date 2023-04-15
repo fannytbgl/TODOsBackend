0c91b2e5a3f1de604404a0dc6a2dd4d7ebba1824
Fatabail <tabaglio.fanny@gmail.com>
2023-04-15 15:32:52 +0200

    Finish controller

--- src/main/java/com/example/backApplication/controller/ToDoController.java
+++ src/main/java/com/example/backApplication/controller/ToDoController.java
@@ -1,14 +1,30 @@
 package com.example.backApplication.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.backApplication.dto.ToDoDto;
+import com.example.backApplication.services.impl.ToDoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/todos")
 public class ToDoController {
 
     // endpoint methods
+    @Autowired
+    private ToDoService toDoService;
 
-    //
+    @GetMapping
+    public List<ToDoDto> getAll() {
+        return this.toDoService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ToDoDto add(@RequestBody ToDoDto toDoDto) {
+        return toDoService.add(toDoDto);
+    }
 
 }
